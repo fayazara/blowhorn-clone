5168700c850417b1ae13e12a62b26f6d227cc681
Fayaz Ahmed <fayazara@gmail.com>
2020-11-08 02:01:37 +0530

    Made the apis

--- api/book.js
+++ api/book.js
@@ -3,10 +3,14 @@ const axios = require("axios");
 module.exports = async (req, res) => {
   try {
     const { body } = req;
-    if (body.name && body.email && body.phone && body.password) {
+    if (body.city && body.pickup && body.drop) {
+      const { data } = await axios.post(
+        "https://jsonbox.io/box_bcbed7702974818a29b3",
+        body
+      );
       res.status(200).send({
         message: "Succesful",
-        details: "Registration Succesful"
+        details: "Your truck is booked"
       });
     } else throw "Missing Data";
   } catch (err) {
